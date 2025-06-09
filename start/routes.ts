e489a0df814541a3454c9a1c26fdcfb672fc69ad
Gui Rios <guilhermeriosprog@gmail.com>
2025-06-09 10:28:02 -0300

    feat:adicionando documetação

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,10 @@
-const UsersController = () => import('#controllers/users_controller')
+import fs from 'node:fs'
 import router from '@adonisjs/core/services/router'
+import swaggerUi from 'swagger-ui-dist'
+import yaml from 'js-yaml'
 import { middleware } from './kernel.js'
+
+const UsersController = () => import('#controllers/users_controller')
 const ObjectsController = () => import('#controllers/objects_controller')
 const SessionControler = () => import('#controllers/session_controller')
 const MeasurementsController = () => import('#controllers/measurements_controller')
@@ -15,20 +19,65 @@ router.group(() => {
   router.post('password/reset-code', [UsersController, 'forgotPassword'])
   router.post('password/validate-code', [UsersController, 'validateResetCode'])
   router.patch('password/reset', [UsersController, 'resetPassword'])
+
   router.post('users', [UsersController, 'store'])
 })
+
 router.resource('user', UsersController).apiOnly()
 router.patch('user/:id/picture', [UsersController, 'uploadProfilePicture'])
-  .use(middleware.fileUpload());
-router
-  .group(() => {
-    router.patch('users/update-token', [UsersController, 'updateNotificationToken'])
+  .use(middleware.fileUpload())
 
-    router.resource('object', ObjectsController).apiOnly()
-    router.resource('object.measurements', MeasurementsController).apiOnly()
-    router.get('object/:object_id/weekly-average', [MeasurementsController, 'weeklyAverage'])
-    router.get('object/:object_id/measurements-latest', [MeasurementsController, 'getLatestMeasurement'])
-    router.patch('object/:id/edit', [ObjectsController, 'edit'])
-    router.patch('object/:id/toggle-connected', [ObjectsController, 'toggleConnected'])
-  })
-  .use(middleware.auth())
+router.group(() => {
+  router.patch('users/update-token', [UsersController, 'updateNotificationToken'])
+
+  router.resource('object', ObjectsController).apiOnly()
+  router.resource('object.measurements', MeasurementsController).apiOnly()
+  
+  router.get('object/:object_id/weekly-average', [MeasurementsController, 'weeklyAverage'])
+  router.get('object/:object_id/measurements-latest', [MeasurementsController, 'getLatestMeasurement'])
+  
+  router.patch('object/:id/edit', [ObjectsController, 'edit'])
+  router.patch('object/:id/toggle-connected', [ObjectsController, 'toggleConnected'])
+})
+.use(middleware.auth())
+
+const SWAGGER_PATH = swaggerUi.getAbsoluteFSPath()
+
+router.get('/docs', async ({ response }) => {
+  const swaggerFile = fs.readFileSync('swagger.yaml', 'utf-8')
+
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Hidro-watch API Docs</title>
+      <link rel="stylesheet" href="/docs/swagger-ui.css" />
+    </head>
+    <body>
+      <div id="swagger-ui"></div>
+      <script src="/docs/swagger-ui-bundle.js" crossorigin></script>
+      <script>
+        window.onload = () => {
+          window.ui = SwaggerUIBundle({
+            spec: ${JSON.stringify(yaml.load(swaggerFile))},
+            dom_id: '#swagger-ui',
+          });
+        };
+      </script>
+    </body>
+    </html>
+  `
+  return response.header('Content-Type', 'text/html').send(html)
+})
+
+router.get('/docs/*', ({ request, response }) => {
+  const path = request.param('*').join('/')
+  const filePath = `${SWAGGER_PATH}/${path}`
+
+  if (fs.existsSync(filePath)) {
+    return response.download(filePath)
+  }
+  return response.notFound()
+})
